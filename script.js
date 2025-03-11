b837c3e3243ede6f9c2a67640343f0e6327dd66b
theodore-brucker <theodore.brucker@maine.edu>
2025-03-11 12:22:39 -0400

    updated about me, added metadata for SEO, improved projects section, reordered sections

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Lazy loading for content sections
-    const sections = ['intro', 'about', 'education', 'experience', 'projects', 'blog'];
+    // Updated order to match the new section order
+    const sections = ['intro', 'about', 'experience', 'projects', 'education', 'blog'];
     sections.forEach(sectionId => {
         const sectionElement = document.getElementById(`${sectionId}-content`);
         loadSectionContent(sectionId, sectionElement);
@@ -45,22 +46,9 @@ function loadSectionContent(sectionId, element) {
                 element.innerHTML = `
                     <div class="about-container">
                         <img src="images/about-me.jpg" alt="Theo Brucker Portrait" class="about-image">
-                        <p>During my bachelors degree I researched Information Privacy and presented my research at an IEEE conference in Hannover, Germany. I enjoy developing security tools and automating security operations. Most recently I passed my CISSP exam, and have been working full time as a tier one SOC where I have gained valuable experience such as automating tasks to drive efficiency and reduce burnout, and detecting and containing real live incidents.</p>
+                        <p>During my bachelors degree I researched Information Privacy and presented my research at an IEEE conference in Hannover, Germany. I enjoy developing security tools and automating security operations. Most recently I passed my CISSP exam, and have earned a promotion to a SOC Analyst II after only 6 months of full time work at a regional MSP in Maine. I spend my time prodiving guidance on security best practices, developing IR playbooks, and responding to security incidents for over 100 clients.</p>
                     </div>
                 `;
-                break;
-            case 'education':
-                element.innerHTML =
-                `<div class="education-container">
-                    <img src="images/associate-of-isc2.png" alt="Theo Brucker ISC2 Associate" class="education-image">
-                    <img src="images/umaine-logo.jpg" alt="University of Maine logo" class="experience-image">
-                    <img src="images/sec-logo.png" alt="CompTIA Security+ logo" class="experience-image">
-                    <p>
-                        ISC2 Associate, 2024 (granted by passing the CISSP exam)<br>
-                        B.S. in Computer Science, University of Maine, 2024<br>
-                        CompTIA Security+, 2023
-                    </p>
-                </div>`
                 break;
             case 'experience':
                 element.innerHTML = 
@@ -76,32 +64,77 @@ function loadSectionContent(sectionId, element) {
                 case 'projects':
                     element.innerHTML = `<ul class="project-list">
                         <li>
-                            [Private]MDR Playbook
-                            <p>Developed and maintained an initial triage and response playbook for an active team of 10 MDR analysts, reducing time to respond and increasing consistency and effectiveness across the board. Follows various incident response standards and focuses specifically on detection, initial response, mitigation, and escalation+triage proesses.</p> 
+                            <span class="project-title">MDR Playbook</span>
+                            <div class="project-tags">
+                                <span class="project-tag">Documentation</span>
+                                <span class="project-tag">Incident Response</span>
+                            </div>
+                            <p>Developed and maintained an initial triage and response playbook for an active team of 10 MDR analysts, reducing time to respond and increasing consistency and effectiveness across the board. Follows various incident response standards and focuses specifically on detection, initial response, mitigation, and escalation+triage processes.</p> 
                         </li>
                         <li>
-                            [Private]MDR Deployment Project
+                            <span class="project-title">MDR Deployments</span>
+                            <div class="project-tags">
+                                <span class="project-tag">Deployment</span>
+                                <span class="project-tag">Team Leadership</span>
+                            </div>
                             <p>Deployed MDR solutions to dozens of clients in under 2 months, including cloud, hybrid, and on-prem topologies. This project was constrained to a strict deadline which I drove for my team to beat by encouraging cross team collaboration and being personally available to troubleshoot or advise on every step of the process.</p>
                         </li>
                         <li>
-                            [Private]Logging Verification Tool
+                            <span class="project-title">Logging Verification Tool</span>
+                            <div class="project-tags">
+                                <span class="project-tag">Automation</span>
+                                <span class="project-tag">Security Monitoring</span>
+                            </div>
                             <p>Developed a tool to verify integrity of logging across 50 clients in under 30 seconds. The previous manual approach to this process took at least 20 hours a month and could not possibly be completed as thoroughly as an automated check. This allowed me to GUARANTEE a level of baseline level visibility into client networks that had never been achieved.</p>
                         </li>
                         <li>
-                            <a href="https://github.com/theodore-brucker/CoveSecurity" target="_blank">[Public]</a>ML-Powered NIDS
-                            <p>A full stack development project for a Network Intrusion Detection System powered by transformer based machine learning techniques to catch extremely complex anomalies at extremely competitive rates on common intrusion and anomaly detection datasets. This project was also an opportunity to practice my DevOps skills, implementing CI/CD strategies and containerization for flexible scaling of resource utilization</p>
+                            <span class="project-title">
+                                <a href="https://github.com/theodore-brucker/CoveSecurity" target="_blank" class="project-link">[Public] ML-Powered NIDS</a>
+                            </span>
+                            <div class="project-tags">
+                                <span class="project-tag">Machine Learning</span>
+                                <span class="project-tag">Network Security</span>
+                                <span class="project-tag">DevOps</span>
+                            </div>
+                            <p>A full stack development project for a Network Intrusion Detection System powered by transformer based machine learning techniques to catch extremely complex anomalies at extremely competitive rates on common intrusion and anomaly detection datasets. This project was also an opportunity to practice my DevOps skills, implementing CI/CD strategies and containerization for flexible scaling of resource utilization.</p>
                         </li>
                         <li>
-                            <a href="https://github.com/theodore-brucker/zig-azure-keyvault-client" target="_blank">[Public]</a>Azure KeyVault Client
+                            <span class="project-title">
+                                <a href="https://github.com/theodore-brucker/zig-azure-keyvault-client" target="_blank" class="project-link">[Public] Azure KeyVault Client</a>
+                            </span>
+                            <div class="project-tags">
+                                <span class="project-tag">API Integration</span>
+                                <span class="project-tag">Security Development</span>
+                            </div>
                             <p>Developed a custom Azure KeyVault client in a low level programming language. This was great exposure to the inner working of the Microsoft Azure API endpoints, and also allowed me to practice my system level coding practices to secure the credentials and authorization tokens used by the tool.</p>
                         </li>
                         <li>
-                            <a href="https://github.com/theodore-brucker/scoutnet" target="_blank">[Public]</a>Scoutnet
+                            <span class="project-title">
+                                <a href="https://github.com/theodore-brucker/scoutnet" target="_blank" class="project-link">[Public] Scoutnet</a>
+                            </span>
+                            <div class="project-tags">
+                                <span class="project-tag">Honeynet</span>
+                                <span class="project-tag">Cloud Security</span>
+                                <span class="project-tag">Threat Intelligence</span>
+                            </div>
                             <p>Developed and deployed a distributed Honeynet server/client infrastructure to collect data on threat actor campaigns targeting Azure hosted resources. This required extensive knowledge of Linux, low level programming, Azure, and cloud security concepts.</p>
                         </li>
                     </ul>`;
                     break;
-                case 'blog':
+            case 'education':
+                element.innerHTML =
+                `<div class="education-container">
+                    <img src="images/associate-of-isc2.png" alt="Theo Brucker ISC2 Associate" class="education-image">
+                    <img src="images/umaine-logo.jpg" alt="University of Maine logo" class="experience-image">
+                    <img src="images/sec-logo.png" alt="CompTIA Security+ logo" class="experience-image">
+                    <p>
+                        ISC2 Associate, 2024 (granted by passing the CISSP exam)<br>
+                        B.S. in Computer Science, University of Maine, 2024<br>
+                        CompTIA Security+, 2023
+                    </p>
+                </div>`
+                break;
+            case 'blog':
                 loadBlogPosts(element);
                 break;
         }
