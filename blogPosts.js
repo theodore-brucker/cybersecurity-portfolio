1c688bf9f25f3093dfc078d1a0cabe2c8892e529
theodore-brucker <theodore.brucker@maine.edu>
2025-03-12 18:10:38 -0400

    adding H1 and canonical tags to improve SEO

--- blogPosts.js
+++ blogPosts.js
@@ -4,46 +4,52 @@ const blogPosts = [
         title: "Discovering my path in cyber",
         preview: "Now that I've broken into the field of cybersecurity, what next?",
         content: `
-            <h2>Discovering my path in cyber</h2>
-
-            <h4><br>What did I do so far</h4>
-            <p>
-            After discovering my passion for coding and having run ins with malicious actors at a young age, I was inspired to go to college to learn more about computers.
-            I was lucky enough to land an internship my sophomore year and later land a job as a SOC technician.
-            This was exactly what I needed, a chance to see how security operations work in the real world.
-            5 months after graduating, I have been improving via trial by fire, and also have a handful of certifications to prove my knowledge in the cybersecurity industry.
-            However, I realize that career progression is not as simple as collecting pieces of paper.
-            </p>
-            
-            <h4><br>What I am going to do next</h4>
-            <p>
-            I have spoken with a few late career cybersecurity professionals who have given me some advice to keep in mind as I move forward.
-            \"Keep your head down and don't ask questions for your first couple years, do good work and pay attention and opportunities will present themselves\" was a particularly powerful piece of advice.
-            Other advice indicated more of the same, with some mention that it is okay to branch out and explore.
-            I am continuing to educate myself, focus on doing good work and earning new responsibilities, and talking to people who have walked the path before me.
-            </p>
-
-            <h4><br>Coming soon</h4>
-            <p>
-            I intend to write a follow up piece to reflect on this post with a fresh perspective at some point in the following months.
-            </p>
-            `
+        <h1>Discovering my path in cyber</h1>
+    
+        <h2>What did I do so far</h2>
+        <p>
+        After discovering my passion for coding and having run ins with malicious actors at a young age, I was inspired to go to college to learn more about computers.
+        I was lucky enough to land an internship my sophomore year and later land a job as a SOC technician.
+        This was exactly what I needed, a chance to see how security operations work in the real world.
+        5 months after graduating, I have been improving via trial by fire, and also have a handful of certifications to prove my knowledge in the cybersecurity industry.
+        However, I realize that career progression is not as simple as collecting pieces of paper.
+        </p>
+        
+        <h2>What I am going to do next</h2>
+        <p>
+        I have spoken with a few late career cybersecurity professionals who have given me some advice to keep in mind as I move forward.
+        "Keep your head down and don't ask questions for your first couple years, do good work and pay attention and opportunities will present themselves" was a particularly powerful piece of advice.
+        Other advice indicated more of the same, with some mention that it is okay to branch out and explore.
+        I am continuing to educate myself, focus on doing good work and earning new responsibilities, and talking to people who have walked the path before me.
+        </p>
+    
+        <h2>Coming soon</h2>
+        <p>
+        I intend to write a follow up piece to reflect on this post with a fresh perspective at some point in the following months.
+        </p>
+        `
     },
     {   id:2,
         title:"How I Passed the CISSP",
         preview:"For those wondering what I did to pass the exam on my first try",
-        content: `<p>On November 21st, 2024, I passed the 10th edition of the CISSP exam at ~115 questions with an hour of time left over. Since then, multiple coworkers and friends asked me various questions about my exam prep, prompting me to write this short post. I’ll start with my knowledge starting point, then discuss the study path I chose. My formal background in computer science started in 2018 during high school and extended up through my bachelor’s in computer science. For cybersecurity in particular, I began in 2022 with a job as a SOC technician at an MSP. Despite being part-time until I finished my bachelor's in 2024, the responsibilities and mentorship I received from my colleagues were indescribable in value. My formal education in computing and information sciences combined with over 2 years of practical experience on a SOC put me in a powerful position to pivot into any corner of cybersecurity.</p><br>
-                  <p>Studying in earnest started when I scheduled the exam 2 months ahead of time, after that I studied between 30 minutes and 1 hour a night before bed, missing only a few nights. I bought How to Think Like a Manager by Luke Ahmed, and the ISC2 Official Study Guide, paying extra for the practice exam book. I read (in varying levels of depth depending on the topic) each chapter of the official study guide, doing the practice questions at the end of each chapter. I worked through the questions in “How to Think like a Manager” as they were extremely in-depth and valuable examples. I would put on Destination Certifications CISSP Mind Map videos to fall asleep to. Fully immersing myself in a topic has always been a surefire strategy for bringing out my best performance. </p><br>
-                  <p>In the last week of my studying before the exam, I scored an average of 80% - 90% on the practice exams. I passed around 115 questions with about an hour leftover.</p><br>
+        content: `
+    <h1>How I Passed the CISSP</h1>
+    
+    <p>On November 21st, 2024, I passed the 10th edition of the CISSP exam at ~115 questions with an hour of time left over. Since then, multiple coworkers and friends asked me various questions about my exam prep, prompting me to write this short post. I'll start with my knowledge starting point, then discuss the study path I chose. My formal background in computer science started in 2018 during high school and extended up through my bachelor's in computer science. For cybersecurity in particular, I began in 2022 with a job as a SOC technician at an MSP. Despite being part-time until I finished my bachelor's in 2024, the responsibilities and mentorship I received from my colleagues were indescribable in value. My formal education in computing and information sciences combined with over 2 years of practical experience on a SOC put me in a powerful position to pivot into any corner of cybersecurity.</p>
 
-                  <p>
-                  I have been an ISC2 Associate for a month now. In retrospect, I am pleased with the way that I took the exam, but there are few lessons learned.
-                  Written lecture notes on videos are great, but they are agonizingly slow when you are dealing with a syllabus as deep as the CISSP.
-                  Thinking like a manager, studying more on the topics I wasn’t confident in, and leveraging my decade of exam taking experience were my strongest weapons.
-                  </p><br>
+    <h2>My Study Approach</h2>
+    <p>Studying in earnest started when I scheduled the exam 2 months ahead of time, after that I studied between 30 minutes and 1 hour a night before bed, missing only a few nights. I bought How to Think Like a Manager by Luke Ahmed, and the ISC2 Official Study Guide, paying extra for the practice exam book. I read (in varying levels of depth depending on the topic) each chapter of the official study guide, doing the practice questions at the end of each chapter. I worked through the questions in "How to Think like a Manager" as they were extremely in-depth and valuable examples. I would put on Destination Certifications CISSP Mind Map videos to fall asleep to. Fully immersing myself in a topic has always been a surefire strategy for bringing out my best performance.</p>
 
-                  <p>All things considered, I am happy that I chose the CISSP. I will be an associate for another couple of years while I work on gaining the remaining experience portion, but the exam is passed and the certification is for life. I have proven my worth and shown yet again that I have what it takes to set a goal in my mind and do what it takes to get there.</p>
-`
+    <h2>Results and Reflections</h2>
+    <p>In the last week of my studying before the exam, I scored an average of 80% - 90% on the practice exams. I passed around 115 questions with about an hour leftover.</p>
+
+    <p>I have been an ISC2 Associate for a month now. In retrospect, I am pleased with the way that I took the exam, but there are few lessons learned.
+    Written lecture notes on videos are great, but they are agonizingly slow when you are dealing with a syllabus as deep as the CISSP.
+    Thinking like a manager, studying more on the topics I wasn't confident in, and leveraging my decade of exam taking experience were my strongest weapons.</p>
+
+    <h2>Moving Forward</h2>
+    <p>All things considered, I am happy that I chose the CISSP. I will be an associate for another couple of years while I work on gaining the remaining experience portion, but the exam is passed and the certification is for life. I have proven my worth and shown yet again that I have what it takes to set a goal in my mind and do what it takes to get there.</p>
+    `
     }
 ];
 
