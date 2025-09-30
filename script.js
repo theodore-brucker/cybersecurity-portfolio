f601f0e370f4326d09e410385c331bc9cebf68e0
theodore-brucker <theodore.brucker@maine.edu>
2025-09-30 17:35:52 -0400

    Added FMI and CMMC experience, updated profile pic and some meta data

--- script.js
+++ script.js
@@ -46,30 +46,46 @@ function loadSectionContent(sectionId, element) {
                 element.innerHTML = `
                     <div class="about-container">
                         <img src="images/about-me.jpg" alt="Theo Brucker Portrait" class="about-image">
-                        <p>During my bachelors degree I researched Information Privacy and presented my research at an IEEE conference in Hannover, Germany. I enjoy developing security tools and automating security operations. Most recently I passed my CISSP exam, and have earned a promotion to a SOC Analyst II after only 6 months of full time work at a regional MSP in Maine. I spend my time prodiving guidance on security best practices, developing IR playbooks, and responding to security incidents for over 100 clients.</p>
+                        <p>While studying for my BsC at UMaine, I researched Information Privacy focused on GDPR compliance for software developers. In my free time I enjoy tinkering with both state of the art and custom developed security tools. Most recently I passed my CISSP exam and accepted a position as a Cybersecurity Specialist at Fiber Materials Inc (FMI) in Biddeford, ME where I write policies and procedures, select and deploy of security tools (SIEM, EDR, AV), advise on secure networking practices, and act as the primary resource for CMMC 2.0 compliance.</p>
                     </div>
                 `;
                 break;
             case 'experience':
                 element.innerHTML = 
                 `<div class="experience-container">
+                    <img src="images/fmi-logo.png" alt="Fiber Materials Inc" class="experience-image">
+                    <p>
+                       Cyber Security Specialist, June 2025 - Present<br>
+                    </p>
+                </div>
+                <div class="experience-container">
                     <img src="images/se-logo.jpeg" alt="Systems Engineering logo" class="experience-image">
                     <p>
-                       Security Analyst II, Systems Engineering, Jan 2025 - Present<br>
-                       Security Analyst I, Systems Engineering, Jan 2023 - Jan 2025<br>
-                       Network Engineering Intern, Systems Engineering, May 2022 - Dec 2022
+                       Security Analyst II, Jan 2025 - June 2025<br>
+                       Security Analyst I, Jan 2023 - Jan 2025<br>
+                       Network Engineering Intern, May 2022 - Dec 2022
                     </p>
                 </div>`;
                 break;
                 case 'projects':
                     element.innerHTML = `<ul class="project-list">
                         <li>
-                            <span class="project-title">MDR Playbook</span>
+                            <span class="project-title">CMMC 2.0 compliance</span>
+                            <div class="project-tags">
+                                <span class="project-tag">Governance, Risk, Compliance</span>
+                                <span class="project-tag">Manufacturing</span>
+                                <span class="project-tag">Project Management</span>
+                                <span class="project-tag">Cybersecurity Operations</span>
+                            </div>
+                            <p>Organization wide lead on compliance with Cybersecurity Maturity Model Certification (CMMC) including policies and procedures, technical implementation of NIST 800-171 R2 controls, evidence collection, executive reporting and advising, and staying up to date on all CMMC 2.0 news and timelines.</p> 
+                        </li>
+                        <li>
+                            <span class="project-title">Incident Response Playbook Development</span>
                             <div class="project-tags">
                                 <span class="project-tag">Documentation</span>
                                 <span class="project-tag">Incident Response</span>
                             </div>
-                            <p>Developed and maintained an initial triage and response playbook for an active team of 10 MDR analysts, reducing time to respond and increasing consistency and effectiveness across the board. Follows various incident response standards and focuses specifically on detection, initial response, mitigation, and escalation+triage processes.</p> 
+                            <p>Developed and maintained a triage and response playbook for an active team of 10 SOC analysts, reducing time to respond and increasing consistency and effectiveness across the board. Follows various incident response standards and focuses specifically on detection, initial response, mitigation, and escalation+triage processes.</p> 
                         </li>
                         <li>
                             <span class="project-title">MDR Deployments</span>
@@ -98,27 +114,6 @@ function loadSectionContent(sectionId, element) {
                             </div>
                             <p>A full stack development project for a Network Intrusion Detection System powered by transformer based machine learning techniques to catch extremely complex anomalies at extremely competitive rates on common intrusion and anomaly detection datasets. This project was also an opportunity to practice my DevOps skills, implementing CI/CD strategies and containerization for flexible scaling of resource utilization.</p>
                         </li>
-                        <li>
-                            <span class="project-title">
-                                <a href="https://github.com/theodore-brucker/zig-azure-keyvault-client" target="_blank" class="project-link">[Public] Azure KeyVault Client</a>
-                            </span>
-                            <div class="project-tags">
-                                <span class="project-tag">API Integration</span>
-                                <span class="project-tag">Security Development</span>
-                            </div>
-                            <p>Developed a custom Azure KeyVault client in a low level programming language. This was great exposure to the inner working of the Microsoft Azure API endpoints, and also allowed me to practice my system level coding practices to secure the credentials and authorization tokens used by the tool.</p>
-                        </li>
-                        <li>
-                            <span class="project-title">
-                                <a href="https://github.com/theodore-brucker/scoutnet" target="_blank" class="project-link">[Public] Scoutnet</a>
-                            </span>
-                            <div class="project-tags">
-                                <span class="project-tag">Honeynet</span>
-                                <span class="project-tag">Cloud Security</span>
-                                <span class="project-tag">Threat Intelligence</span>
-                            </div>
-                            <p>Developed and deployed a distributed Honeynet server/client infrastructure to collect data on threat actor campaigns targeting Azure hosted resources. This required extensive knowledge of Linux, low level programming, Azure, and cloud security concepts.</p>
-                        </li>
                     </ul>`;
                     break;
             case 'education':
@@ -128,7 +123,7 @@ function loadSectionContent(sectionId, element) {
                     <img src="images/umaine-logo.jpg" alt="University of Maine logo" class="experience-image">
                     <img src="images/sec-logo.png" alt="CompTIA Security+ logo" class="experience-image">
                     <p>
-                        ISC2 Associate, 2024 (granted by passing the CISSP exam)<br>
+                        Associate CISSP, November 2024<br>
                         B.S. in Computer Science, University of Maine, 2024<br>
                         CompTIA Security+, 2023
                     </p>
